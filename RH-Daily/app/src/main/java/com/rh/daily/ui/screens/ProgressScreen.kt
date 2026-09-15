11975c51504c77d54e1207e260563792417de7af
Renaldi Husada <renaldihusada2828@gmail.com>
2026-09-16 03:34:01 +0700

    fix ProgressScreen compose state

--- RH-Daily/app/src/main/java/com/rh/daily/ui/screens/ProgressScreen.kt
+++ RH-Daily/app/src/main/java/com/rh/daily/ui/screens/ProgressScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
